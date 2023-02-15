1c02b6d82231fe46e3d2733a3c53d534daa57257
Beatrix <beatrix@sourcegraph.com>
2023-02-15 09:21:38 -0800

    Verify first level of overlays in examples

--- .buildkite/verify-overlays.sh
+++ .buildkite/verify-overlays.sh
@@ -4,7 +4,7 @@ set -eu
 pushd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 OVERLAYS=()
-mapfile -t OVERLAYS < <(find examples -maxdepth 2 -type d ! -name '.*' ! -name 'monitoring' ! -name 'ingress-controller' ! -name 'examples' -printf '%f\n')
+mapfile -t OVERLAYS < <(find examples -maxdepth 1 -type d ! -name '.*' ! -name 'monitoring' ! -name 'ingress-controller' ! -name 'examples' -printf '%f\n')
 
 for O in "${OVERLAYS[@]}"; do
   echo "<<<< GENERATING ${O} OVERLAY >>>>>"
