4561a59efa5583bd494e2fb3b8bbdc6ef6d0b779
Beatrix <beatrix@sourcegraph.com>
2023-02-15 07:21:37 -0800

    add tools

--- .buildkite/check-image-names.sh
+++ .buildkite/check-image-names.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 ROOT="$(pwd)"
 
-pushd tools/check-image-names
+pushd .buildkite/tools/check-image-names
 
 echo "--- Check to see if all manifests contain valid image names"
 go run check-image-names.go "${ROOT}"/base
