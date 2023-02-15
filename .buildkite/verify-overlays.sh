321dbb9ed8b2bbcb2ed6b2d9b4033c25d3e5afe3
Beatrix <beatrix@sourcegraph.com>
2023-02-15 09:09:22 -0800

    add go mod and sum

--- .buildkite/verify-overlays.sh
+++ .buildkite/verify-overlays.sh
@@ -4,11 +4,11 @@ set -eu
 pushd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 OVERLAYS=()
-mapfile -t OVERLAYS < <(find examples -maxdepth 2 -type d ! -name '.*' ! -name 'monitoring' ! -name 'ingress-controller' -printf '%f\n')
+mapfile -t OVERLAYS < <(find examples -maxdepth 2 -type d ! -name '.*' ! -name 'monitoring' ! -name 'ingress-controller' ! -name 'examples' -printf '%f\n')
 
 for O in "${OVERLAYS[@]}"; do
   echo "<<<< GENERATING ${O} OVERLAY >>>>>"
-  DIR=generated-cluster
+  DIR=ci-generated-cluster
   mkdir "${DIR}"
   kubectl kustomize examples/"${O}" -o "${DIR}"
   rm -rf "${DIR}"
--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -6,6 +6,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 gcloud container clusters get-credentials cloud --zone us-central1-f --project sourcegraph-dev
 
 kubectl kustomize ./base/. | kubectl apply --dry-run --validate --context=gke_sourcegraph-dev_us-central1-f_cloud -f -
-kubectl apply --dry-run --validate --recursive -f configure/ --context=gke_sourcegraph-dev_us-central1-f_cloud
 
 .buildkite/verify-label.sh
